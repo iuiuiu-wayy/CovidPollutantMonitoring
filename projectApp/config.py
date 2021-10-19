f9aba831765f7e2af00369d1c16ebb04fe233bdd
iuiuiu-wayy <42693503+iuiuiu-wayy@users.noreply.github.com>
2021-10-19 13:51:16 +0700

    Delete config.py

--- projectApp/config.py
+++
@@ -1,6 +0,0 @@
-class Config:
-    #database
-    # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:project@localhost/ProjectDB'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://dwkdfcdeibnelx:51a785d4d4b0b45b318600ced25c6fd6f566569c4d46576d81cfd58078b52c7f@ec2-176-34-105-15.eu-west-1.compute.amazonaws.com:5432/d7gabukuo9iol4'
-    SQLALCHEMY_ECHO = False
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
